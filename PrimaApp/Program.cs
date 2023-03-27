be1d48df525601cfa7922aa3520a32f55491ff6d
Fabio Mattesini <fabiom222@hotmail.it>
2023-03-27 12:35:28 +0200

    Aggiungere i file di progetto.

---
+++ PrimaApp/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.Clear();
+        string? nome = "da inserire";
+        int slm = int.Parse("1");
+        Console.WriteLine("Come ti chiami?");
+        nome = Console.ReadLine();
+        Console.WriteLine("Ciao " + nome + "!");
+        
+    }
+}
