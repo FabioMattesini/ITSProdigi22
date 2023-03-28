af72e9ea4732d9874f2e4f6f6706189d6063c4f0
Fabio Mattesini <fabiom222@hotmail.it>
2023-03-28 12:49:04 +0200

    Aggiunta classe Utility

--- PrimaApp/Program.cs
+++ PrimaApp/Program.cs
@@ -1,47 +1,34 @@
-﻿internal class Program
+﻿using PrimaApp;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.BackgroundColor = ConsoleColor.DarkBlue;
         Console.Clear();
-        //impostiamo la base delle nostra tabellina
-        string richiesta = chiedi("Che tabellina vuoi?");
-        int baseTabellina = int.Parse(richiesta);
-        //decidiamo quante volte eseguire la moltiplicazione
-        richiesta = "Quante volte vuoi ripetere la tabellina?";
-        int volte = chiediNumero(richiesta) + 1;
+        bool ripeti = true;
         
-        for (int i = 0; i < volte; i++)
-        {
-            int risultato = i * baseTabellina;
-            Console.WriteLine($"{i}\t*\t{baseTabellina}\t=\t{risultato}");
-            //Console.WriteLine(i.ToString() + " * " + baseTabellina.ToString() + " = " + risultato.ToString());
+        while (ripeti) {
+            //impostiamo la base delle nostra tabellina
+            string richiesta = Utility.chiedi("Che tabellina vuoi?");
+            int baseTabellina = int.Parse(richiesta);
+            //decidiamo quante volte eseguire la moltiplicazione
+            richiesta = "Quante volte vuoi ripetere la tabellina?";
+            int volte = Utility.chiediNumero(richiesta) + 1;
+
+            for (int i = 0; i < volte; i++)
+            {
+                int risultato = i * baseTabellina;
+                Console.WriteLine($"{i}\t*\t{baseTabellina}\t=\t{risultato}");
+                //Console.WriteLine(i.ToString() + " * " + baseTabellina.ToString() + " = " + risultato.ToString());
+            }
+
+            string risposta = Utility.chiedi("Vuoi un'altra tabellina? (s/n)");
+            ripeti = risposta == "s";
         }
+
+        Console.WriteLine("Buona giornata!");
     }
 
-    /// <summary>
-    /// funzione per porre una domanda all'utente tramite la console
-    /// </summary>
-    /// <param name="domanda">La domanda da porre all'utente</param>
-    /// <returns>Quello che l'utente ha scritto in console</returns>
-    private static string chiedi(string domanda)
-    {
-        //scrivo la domanda
-        Console.Write(domanda + ": ");
-        //recupero dal buffer la risposta
-        string risposta = Console.ReadLine();
-        return risposta;
-    }
-
-    /// <summary>
-    /// funzione per chiedere un numero all'utente
-    /// </summary>
-    /// <param name="domanda">La domanda da porre all'utente</param>
-    /// <returns>Il numero che l'utente ha scritto in console</returns>
-    private static int chiediNumero(string domanda)
-    {
-        Console.Write(domanda + ": ");
-        int risposta = int.Parse(Console.ReadLine());
-        return risposta;
-    }
+    
 }
---
+++ PrimaApp/Utility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimaApp
+{
+    internal class Utility
+    {
+        /// <summary>
+        /// funzione per porre una domanda all'utente tramite la console
+        /// </summary>
+        /// <param name="domanda">La domanda da porre all'utente</param>
+        /// <returns>Quello che l'utente ha scritto in console</returns>
+        public static string chiedi(string domanda)
+        {
+            //scrivo la domanda
+            Console.Write(domanda + ": ");
+            //recupero dal buffer la risposta
+            string risposta = Console.ReadLine();
+            return risposta;
+        }
+
+        /// <summary>
+        /// funzione per chiedere un numero all'utente
+        /// </summary>
+        /// <param name="domanda">La domanda da porre all'utente</param>
+        /// <returns>Il numero che l'utente ha scritto in console</returns>
+        public static int chiediNumero(string domanda)
+        {
+            Console.Write(domanda + ": ");
+            int risposta = int.Parse(Console.ReadLine());
+            return risposta;
+        }
+    }
+}
