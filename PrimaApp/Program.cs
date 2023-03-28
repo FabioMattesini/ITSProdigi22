ceb92ec8248e071dd1c0f4b4722d95563abb1329
Fabio Mattesini <fabiom222@hotmail.it>
2023-03-28 10:08:43 +0200

    Aggiunta richiesta input da utente

--- PrimaApp/Program.cs
+++ PrimaApp/Program.cs
@@ -5,15 +5,30 @@
         Console.BackgroundColor = ConsoleColor.DarkBlue;
         Console.Clear();
         //impostiamo la base delle nostra tabellina
-        int baseTabellina = 2;
+        string richiesta = chiedi("Che tabellina vuoi?");
+        int baseTabellina = int.Parse(richiesta);
         //decidiamo quante volte eseguire la moltiplicazione
-        int volte = 10;
+        int volte = 11;
         
         for (int i = 0; i < volte; i++)
         {
             int risultato = i * baseTabellina;
-            Console.WriteLine($"{i} * {baseTabellina} = {risultato}");
+            Console.WriteLine($"{i}\t*\t{baseTabellina}\t=\t{risultato}");
             //Console.WriteLine(i.ToString() + " * " + baseTabellina.ToString() + " = " + risultato.ToString());
         }
+    }
+
+    /// <summary>
+    /// funzione per porre una domanda all'utente tramite la console
+    /// </summary>
+    /// <param name="domanda">La domanda da porre all'utente</param>
+    /// <returns>Quello che l'utente ha scritto in console</returns>
+    private static string chiedi(string domanda)
+    {
+        //scrivo la domanda
+        Console.Write(domanda + ": ");
+        //recupero dal buffer la risposta
+        string risposta = Console.ReadLine();
+        return risposta;
     }
 }
