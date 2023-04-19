3cd0d7e7d71f559a0a295be6ac7b68029fcb4e0e
Fabio Mattesini <fabiom222@hotmail.it>
2023-04-19 16:59:34 +0200

    fewfw

--- Rubrica/Program.cs
+++ Rubrica/Program.cs
@@ -2,9 +2,16 @@
 {
     internal class Program
     {
+        static private List<Contatto> contatti;
         static void Main(string[] args)
         {
+            contatti = new List<Contatto>();
+        }
 
+        static private string chiedi(string domanda)
+        {
+            Console.Write($"{domanda} :");
+            return Console.ReadLine();
         }
     }
 }
