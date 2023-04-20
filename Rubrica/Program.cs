6811bc4048875045363f07f1453bbc88d867fd7f
Fabio Mattesini <fabiom222@hotmail.it>
2023-04-20 09:11:50 +0200

    update

--- Rubrica/Program.cs
+++ Rubrica/Program.cs
@@ -7,13 +7,35 @@
         {
             contatti = new List<Contatto>();
             string comando = "";
+            do
+            {
+                comando = chiedi("cosa vuoi fare?");
+                switch (comando)
+                {
+                    case "nuovo":
+                        break;
+                    case "cancella":
+                        break;
+                    case "modifica":
+                        break;
+                    case "vedi":
+                        break;
+                    case "salva":
+                        break;
+                    case "apri":
+                        break;
+                    default:
+                        Console.WriteLine("non ho capito...");
+                        break;
+                }
 
+            } while (comando != "exit");
         }
 
         static private string chiedi(string domanda)
         {
             Console.Write($"{domanda} :");
-            return Console.ReadLine();
+            return Console.ReadLine().ToLower().Trim();
         }
     }
 }
