f0b3ae01a7181f8c767b7296cfc999d592bca914
Fabio Mattesini <fabiom222@hotmail.it>
2023-04-27 16:37:40 +0200

    Implementate funzioni nuovo, vedi, salva e apri

--- Rubrica/Program.cs
+++ Rubrica/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Rubrica
+﻿using System.Text.Json;
+
+namespace Rubrica
 {
     internal class Program
     {
@@ -6,23 +8,53 @@
         static void Main(string[] args)
         {
             contatti = new List<Contatto>();
-            string comando = "";
+            string comando;
             do
             {
                 comando = chiedi("cosa vuoi fare?");
                 switch (comando)
                 {
-                    case "nuovo":
+                    case "nuovo": //Create
+                        contatti.Add(
+                                new(
+                                        chiedi("Nome:", false),
+                                        chiedi("Cognome:", false),
+                                        chiedi("Telefono:", false)
+                                    )
+                            );
+                        Console.WriteLine($"Contatti presenti: {Contatto.quanti}");
                         break;
+                    case "vedi": //Read
+                        string chi = chiedi("Chi devo cercare?", false);
+                        List<Contatto> selezionati = contatti.Where(x => x.nome.Contains(chi)).ToList();
+                        foreach (Contatto singolo in selezionati)
+                        {
+                            Console.WriteLine(singolo);
+                        }
+                        break;
+                        //TODO
                     case "cancella":
                         break;
+                        //TODO
                     case "modifica":
                         break;
-                    case "vedi":
+                     
+                    case "salva": //salvo la rubrica serializzandola su un file JSON
+                        File.WriteAllText("rubrica.json", JsonSerializer.Serialize(contatti));
+                        Console.WriteLine("Rubrica salvata su disco!");
                         break;
-                    case "salva":
-                        break;
-                    case "apri":
+                    case "apri": //Deserializzo il file JSON
+                        try
+                        {
+                            string buffer = File.ReadAllText("rubrica.json");
+                            contatti = JsonSerializer.Deserialize<List<Contatto>>(buffer);
+                            Console.WriteLine($"Caricati {contatti.Count} contatti");
+                        }
+                        catch
+                        {
+                            contatti = new();
+                            Console.WriteLine("Errore di caricamento del file!");
+                        }
                         break;
                     default:
                         Console.WriteLine("non ho capito...");
@@ -32,10 +64,13 @@
             } while (comando != "exit");
         }
 
-        static private string chiedi(string domanda)
+        static private string chiedi(string domanda, bool corretto = true)
         {
-            Console.Write($"{domanda} :");
-            return Console.ReadLine().ToLower().Trim();
+            Console.Write($"{domanda}");
+            if (corretto)
+                return Console.ReadLine().ToLower().Trim();
+            else
+                return Console.ReadLine();
         }
     }
 }
