d807a89eba9d625ef6721ca21fc392d26f187224
Fabio Mattesini <fabiom222@hotmail.it>
2023-05-03 15:30:11 +0200

    Creazione progetto ereditarietà

---
+++ CatalogoInteressi/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CatalogoInteressi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] semplice = { };
+            List<int> complesso = new List<int>();
+            IEnumerable<int> generico = complesso;
+            generico = semplice;
+        }
+    }
+}
