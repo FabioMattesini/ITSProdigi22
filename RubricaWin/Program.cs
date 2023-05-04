52b4cd35fdc898267dc769ebfd7dbb72b7bd562c
Fabio Mattesini <fabiom222@hotmail.it>
2023-05-04 09:37:42 +0200

    Rubrica su WIndows Form

--- RubricaWin/Program.cs
+++ RubricaWin/Program.cs
@@ -6,12 +6,15 @@ namespace RubricaWin
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] argomenti)
         {
+            string fileDaAprire = "rubricaForm.json";
+            if(argomenti.Length == 1)
+                fileDaAprire = argomenti[0];
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Rubrica());
+            Application.Run(new Rubrica(fileDaAprire));
         }
     }
 }
