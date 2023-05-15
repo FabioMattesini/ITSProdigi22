3a7e98794db6a0cf20880c0213a78a63cc293360
Fabio Mattesini <fabiom222@hotmail.it>
2023-05-15 13:53:29 +0200

    Implementata ricerca di link all'interno di pagina wikipedia

--- EmailScavenger/Form1.cs
+++ EmailScavenger/Form1.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
 
 namespace EmailScavenger
 {
@@ -18,12 +19,24 @@ namespace EmailScavenger
             HttpResponseMessage risposta = await browser.GetAsync(url);
             if (risposta.StatusCode == HttpStatusCode.OK)
             {
-                foreach(var elemento in risposta.Content.Headers) //gli elementi degli Headers sono delle coppie chiave/valore, dove il valore è un Enumerable di string
+                string corpo = await risposta.Content.ReadAsStringAsync();
+                string pattern = @"a href=""([^""]+)";
+                MatchCollection links = Regex.Matches(corpo, pattern, RegexOptions.Multiline);
+                foreach (Match link in links) 
                 {
-                    string chiave = elemento.Key;
-                    string valore = string.Join(", ", elemento.Value); //unisce tutti i valori relativi alla chiave considerata
-                    lstRisultati.Items.Add($"{chiave}:\t{valore}");
+                    string indirizzo = link.Groups[1].Value;
+                    if(!indirizzo.StartsWith("http"))
+                        indirizzo = url + indirizzo;
+                    if(!lstRisultati.Items.Contains(indirizzo))
+                        lstRisultati.Items.Add(indirizzo);
                 }
+
+                //foreach(var elemento in risposta.Content.Headers) //gli elementi degli Headers sono delle coppie chiave/valore, dove il valore è un Enumerable di string
+                //{
+                //    string chiave = elemento.Key;
+                //    string valore = string.Join(", ", elemento.Value); //unisce tutti i valori relativi alla chiave considerata
+                //    lstRisultati.Items.Add($"{chiave}:\t{valore}");
+                //}
             }
         }
     }
