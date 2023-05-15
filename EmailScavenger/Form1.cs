7d4c1f2de68d7b897e58bd6164407d3c0cda0d71
Fabio Mattesini <fabiom222@hotmail.it>
2023-05-15 12:15:26 +0200

    Aggiunto commento

--- EmailScavenger/Form1.cs
+++ EmailScavenger/Form1.cs
@@ -18,10 +18,10 @@ namespace EmailScavenger
             HttpResponseMessage risposta = await browser.GetAsync(url);
             if (risposta.StatusCode == HttpStatusCode.OK)
             {
-                foreach(var elemento in risposta.Content.Headers)
+                foreach(var elemento in risposta.Content.Headers) //gli elementi degli Headers sono delle coppie chiave/valore, dove il valore è un Enumerable di string
                 {
                     string chiave = elemento.Key;
-                    string valore = string.Join(", ", elemento.Value);
+                    string valore = string.Join(", ", elemento.Value); //unisce tutti i valori relativi alla chiave considerata
                     lstRisultati.Items.Add($"{chiave}:\t{valore}");
                 }
             }
